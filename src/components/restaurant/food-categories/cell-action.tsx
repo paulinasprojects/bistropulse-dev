8441f549f548986303a87e815ce5c95e4e1a1ab0
paulinasprojects <virtualpaula93@gmail.com>
2025-03-07 13:37:00 +0200

    added edit category modal

--- src/components/restaurant/food-categories/cell-action.tsx
+++ src/components/restaurant/food-categories/cell-action.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Pencil,Trash2 } from "lucide-react";
 import { FoodCategoriesListProps } from "./columns";
 import { DeleteCategoryModal } from "./delete-category-modal";
+import { EditCategoryModal } from "./edit-category-modal";
 
 interface Props {
   data: FoodCategoriesListProps;
@@ -12,15 +13,20 @@ interface Props {
 
 export const CellAction = ({ data }: Props) => {
   const [openDeleteCategoryModal, setOpenDeleteCategoryModal] = useState<boolean>(false);
+  const [openEditCategoryModal, setOpenEditCategoryModal] = useState<boolean>(false);
 
   const handleOpen = () => {
     setOpenDeleteCategoryModal(!openDeleteCategoryModal)
   }
 
+  const handleEditModalOpen = () => {
+    setOpenEditCategoryModal(!openEditCategoryModal)
+  }
+
   return (
     <div className="">
       <div className="flex gap-4">
-        <Pencil className="size-5 cursor-pointer"/>
+        <Pencil className="size-5 cursor-pointer" onClick={() => handleEditModalOpen()}/>
         <Trash2 className="size-5 cursor-pointer" onClick={() => handleOpen()}/>
       </div>
       {openDeleteCategoryModal && (
@@ -31,6 +37,16 @@ export const CellAction = ({ data }: Props) => {
           className="add-category-modal"
         >
           <DeleteCategoryModal setOpenModal={handleOpen}/>
+        </motion.div>
+      )}
+      {openEditCategoryModal && (
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{  opacity: 1, y: 0}}
+          transition={{duration: 0.2}}
+          className="add-category-modal"
+        >
+          <EditCategoryModal setOpenModal={handleEditModalOpen} name={data.name}/>
         </motion.div>
       )}
     </div>
---
+++ src/components/restaurant/food-categories/edit-category-modal.tsx
@@ -0,0 +1,25 @@
+import { Separator } from "@/components/ui/separator";
+import { X } from "lucide-react";
+
+
+interface Props {
+  setOpenModal: () => void;
+  name: string;
+}
+
+export const EditCategoryModal = ({ setOpenModal, name }: Props) => {
+  return (
+    <div className="test-modal">
+      <div className="flex items-center justify-between mx-5">
+        <span className="text-xl font-semibold text-[#1A1D1F]">Edit Food Category Name</span>
+        <X className="size-5 cursor-pointer" onClick={setOpenModal}/>
+      </div>
+      <Separator className="mt-6"/>
+      <div className="mt-6 flex flex-col gap-2">
+        <label htmlFor="categoryEditInput" className="text-sm font-semibold text-[#1A1D1F">Category Name</label>
+        <input id="categoryEditInput" type="text" defaultValue={name} placeholder="Enter a new name" className="w-full border-[1px] border-[#9A9FA5] rounded-[6px] h-[48px] pl-5" />
+        <button type='submit' className='mt-3 w-full bg-[#2A85FF] hover:bg-[#4291f8] text-white h-[48px] rounded-[6px] text-[15px] font-semibold' onClick={setOpenModal}>Submit</button>
+      </div>
+    </div>
+  )
+}
