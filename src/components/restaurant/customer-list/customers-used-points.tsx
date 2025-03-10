a8879d370a698c5d1a38a2027f69cfab217bfb37
paulinasprojects <virtualpaula93@gmail.com>
2025-03-10 15:58:41 +0200

    added responsive customers used  points section

--- src/components/restaurant/customer-list/customers-used-points.tsx
+++ src/components/restaurant/customer-list/customers-used-points.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { CardHeading } from "@/components/card-heading";
+import { Separator } from "@/components/ui/separator";
 
 export const CustomersUsedPoints = () => {
   return (
@@ -8,6 +9,65 @@ export const CustomersUsedPoints = () => {
         <CardHeading
           title="Used Points"
         />
+        <div className="mt-6">
+          <div className="flex gap-3">
+            <div className="w-[44px] h-[44px] bg-[#219653] rounded-full">
+              <p className="text-white text-[11px] font-bold text-center mt-1.5">Earn Cash</p>
+            </div>
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-sm font-medium">Get $50 on your mobile wallet</p>
+              <p className="text-sm font-normal text-[#2F80ED]">1000 Points</p>
+            </div>
+          </div>
+            <Separator className="my-4"/>
+        </div>
+        <div className="">
+          <div className="flex gap-3">
+            <div className="w-[44px] h-[44px] bg-[#219653] rounded-full">
+              <p className="text-white text-[11px] font-bold text-center mt-1.5">Earn Cash</p>
+            </div>
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-sm font-medium">Get $50 on your mobile wallet</p>
+              <p className="text-sm font-normal text-[#2F80ED]">1000 Points</p>
+            </div>
+          </div>
+            <Separator className="my-4"/>
+        </div>
+        <div className="">
+          <div className="flex gap-3">
+            <div className="w-[44px] h-[44px] bg-[#219653] rounded-full">
+              <p className="text-white text-[11px] font-bold text-center mt-1.5">Earn Cash</p>
+            </div>
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-sm font-medium">Get $50 on your mobile wallet</p>
+              <p className="text-sm font-normal text-[#2F80ED]">1000 Points</p>
+            </div>
+          </div>
+            <Separator className="my-4"/>
+        </div>
+        <div className="">
+          <div className="flex gap-3">
+            <div className="w-[44px] h-[44px] bg-[#219653] rounded-full">
+              <p className="text-white text-[11px] font-bold text-center mt-1.5">Earn Cash</p>
+            </div>
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-sm font-medium">Get $50 on your mobile wallet</p>
+              <p className="text-sm font-normal text-[#2F80ED]">1000 Points</p>
+            </div>
+          </div>
+            <Separator className="my-4"/>
+        </div>
+        <div className="">
+          <div className="flex gap-3">
+            <div className="w-[44px] h-[44px] bg-[#219653] rounded-full">
+              <p className="text-white text-[11px] font-bold text-center mt-1.5">Earn Cash</p>
+            </div>
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-sm font-medium">Get $50 on your mobile wallet</p>
+              <p className="text-sm font-normal text-[#2F80ED]">1000 Points</p>
+            </div>
+          </div>
+        </div>
       </CardContent>
     </Card>
   )
